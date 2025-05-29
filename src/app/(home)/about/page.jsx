5663c3436f50f1fba6d8b9570d4ef15eaa169622
minhyeong <minhyeong.dev@gmail.com>
2025-05-29 18:04:52 +0900

    fix: no use header

--- src/app/(home)/about/page.jsx
+++ src/app/(home)/about/page.jsx
@@ -1,11 +1,7 @@
-import Header from "../../../components/(home)/.common/organism/Header.organism";
 import Curriculum from "../../../components/(home)/about/template/Some.template";
 
 export default function Page() {
 	return (
-		<div>
-			<Header/>
-			<Curriculum/>
-		</div>
+		<Curriculum />
 	)
 }
