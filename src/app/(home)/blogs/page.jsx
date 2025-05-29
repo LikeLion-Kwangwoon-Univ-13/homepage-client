b794a8bd5a3da5d894e5198b68185c691beb9ab5
minhyeong <minhyeong.dev@gmail.com>
2025-05-29 18:32:45 +0900

    fix: no header

--- src/app/(home)/blogs/page.jsx
+++ src/app/(home)/blogs/page.jsx
@@ -1,4 +1,3 @@
-import Header from "../../../components/(home)/.common/organism/Header.organism";
 import BlogHighlightSection from "../../../components/(home)/blogs/template/Some.template";
 import { Routes, Route } from "react-router-dom";
 import BlogAllPage from "./all/page.jsx";
@@ -6,7 +5,6 @@ import BlogAllPage from "./all/page.jsx";
 export default function BlogPage() {
   return (
     <>
-      <Header />
       <Routes>
         <Route index element={<BlogHighlightSection />} />
         <Route path="all" element={<BlogAllPage />} />
