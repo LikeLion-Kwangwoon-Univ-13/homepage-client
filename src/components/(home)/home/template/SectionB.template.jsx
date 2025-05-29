bab068dca9378ef7d42a3153b41780da2f4a10da
minhyeong <minhyeong.dev@gmail.com>
2025-05-29 17:52:58 +0900

    feat: test ```Input``` widget

--- src/components/(home)/home/template/SectionB.template.jsx
+++ src/components/(home)/home/template/SectionB.template.jsx
@@ -1,11 +1,19 @@
 import { cn } from "@/utils"
 import style from "../../style"
+import Input from "../../_widget/Input"
+import { useState } from "react"
 export default function SectionB() {
 	const container = {
 		size: style.size,
 		text: 'text-white'
 	}
+	const [text, setText] = useState()
 	return (
-		<div className={cn(container)}>SectionB</div>
+		<div className={cn(container)}>
+			<Input state={[text, setText]} placeholder="이름" onKeyDown={(e) => {
+				if (e.key === 'Enter') return alert(text)
+			}
+			} />
+		</div>
 	)
 }
