63534c86eb8e696306f467378a95acb705525875
EunBin <rhdmsqls1234@naver.com>
2025-07-06 11:20:15 +0900

    fix: project page detail

--- src/components/(home)/project/template/list/ProjectListCard.jsx
+++ src/components/(home)/project/template/list/ProjectListCard.jsx
@@ -20,7 +20,7 @@ const ProjectListCard = ({ title, type, description, image, isFixedSize }) => {
   return (
     <div onClick={handleClick} className={`rounded-[24px] p-[34px] text-white cursor-pointer border border-white-1px ${cardClasses}`}>
       <div className="flex items-center justify-center min-w-[375px] aspect-[375/211] rounded-[24px] border border-white-1px">
-        <img src={image} alt="project-image" className={` ${imageClasses} opacity-30`} />
+        <img src={image} alt="project-image" />
       </div>
       <div style={{fontFamily: 'Space-Grotesk', fontSize: '24px'}}className="mt-[32px] font-bold">{title} <span style={{fontFamily: 'Space-Grotesk', fontSize: '20x'}} className="ml-[20px] text-gray-400">{type}</span></div>
       <div style={{fontFamily: 'Space-Grotesk', fontSize: '20px'}} className="mt-[32px] whitespace-nowrap overflow-hidden text-ellipsis">{description}</div>
