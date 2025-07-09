631bfbd694ddfe29d15733e4553f86852d5f0391
EunBin <rhdmsqls1234@naver.com>
2025-07-09 22:52:55 +0900

    feat: project detail api

--- src/components/(home)/project/template/main/Some.template.jsx
+++ src/components/(home)/project/template/main/Some.template.jsx
@@ -33,8 +33,15 @@ const ProjectMain = () => {
     fontSize: "220px",
     fontWeight: 400,
     textShadow: "0px 4px 25px rgba(229, 50, 15, 0.5)",
-    animation: "slideX 10s ease-in-out infinite alternate",
+    animation: "slideLeft 25s linear infinite",
   };
+console.log("data:", data);
+console.log("data.projects:", data?.projects);
+
+  const projectsToShow =(() => {
+    if (!data || !Array.isArray(data.projects)) return [];
+    return data.projects.slice(0, 5);
+  })();
 
   if (isLoading) return <p style={{fontFamily: 'Space Grotesk', fontSize: '20px'}} className="text-white">로딩 중...</p>;
 
@@ -48,12 +55,12 @@ const ProjectMain = () => {
   return (
     <>
       <style>{`
-        @keyframes slideX {
+        @keyframes slideLeft {
           0% {
-            transform: translateX(2%);
+            transform: translateX(0%);
           }
           100% {
-            transform: translateX(-10%);
+            transform: translateX(-50%);
           }
         }
       `}</style>
@@ -61,13 +68,13 @@ const ProjectMain = () => {
       <div className="flex-1 min-h-screen">
         <div style={containerStyle}>
           <div style={textStyle}>
-            OUR PROJECTS - EXPLORE<span style={{ display: "inline-block", width: "200px" }}></span>OUR PROJECTS - EXPLORE
+            OUR PROJECTS - EXPLORE<span style={{ display: "inline-block", width: "200px" }}></span>OUR PROJECTS - EXPLORE<span style={{ display: "inline-block", width: "200px" }}></span>OUR PROJECTS - EXPLORE<span style={{ display: "inline-block", width: "200px" }}></span>OUR PROJECTS - EXPLORE
           </div>
         </div>
 
         <img src="/images/project-bg.png" className="fixed bottom-0 left-1/2 -translate-x-1/2 w-[100vw] h-[467px] z-[-1/2] pointer-events-none select-none" />
 
-        <ProjectSection projects={data?.projects?.slice(0,5) || []}/>
+        <ProjectSection projects={projectsToShow}/>
       </div>
       <ProjectFooter/>
     </>
